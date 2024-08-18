42b935abf752ec7485053de57db6580a96086a08
Optimist0511 <Optimist050511@Gmail.com>
2024-08-18 22:08:34 +0800

    update

--- src/app/constant.ts
+++ src/app/constant.ts
@@ -1,5 +1,5 @@
-export const OWNER = "Yidadaa";
-export const REPO = "ChatGPT-Next-Web";
+export const OWNER = "Optimist0511";
+export const REPO = "my-gpt-1";
 export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,20 +8,23 @@ import { type Metadata } from "next";
 export const metadata: Metadata = {
   title: "NextChat",
   description: "Your personal ChatGPT Chat Bot.",
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-    maximumScale: 1,
-  },
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "#fafafa" },
-    { media: "(prefers-color-scheme: dark)", color: "#151515" },
-  ],
   appleWebApp: {
     title: "NextChat",
     statusBarStyle: "default",
   },
 };
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+};
+
+export const themeColor = [
+  { media: "(prefers-color-scheme: light)", color: "#fafafa" },
+  { media: "(prefers-color-scheme: dark)", color: "#151515" },
+];
+
 
 export default function RootLayout({
   children,
